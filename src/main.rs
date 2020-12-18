16b1cdf830399fbc55a0f80493e18516730fbbbf
Jaret Screws <jaretscrews@gmail.com>
2020-12-18 01:05:07 -0500

    scrape amazon to get the price

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,6 @@ use wanted_thing::WantedThing;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("https://httpbin.org/ip")
-        .await?
-        .json::<HashMap<String, String>>()
-        .await?;
-    println!("{:#?}", resp);
-
     let test_item: WantedThing = WantedThing::new(
         "Watch Dogs Legion".to_string(), 
         "https://www.amazon.com/Watch-Dogs-Legion-PlayStation-5-Standard/dp/B08FS6BB9N/ref=sr_1_2?dchild=1&keywords=ps5+game&qid=1608266021&sr=8-2".to_string(), 
@@ -21,6 +15,5 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     );
 
     test_item.details();
-    test_item.check_url().await?;
     Ok(())
 }
--- src/wanted_thing.rs
+++ src/wanted_thing.rs
@@ -3,39 +3,47 @@ use chrono::{DateTime, Duration};
 use chrono::offset::Utc;
 use reqwest;
 use scraper::{Html, Selector};
+use rusty_money::Money;
+use futures::executor::block_on;
 
 pub struct WantedThing {
     name: String,
-    url: Url,
+    item_url: Url,
+    price: Money,
     time_to_check: DateTime<Utc>,
 }
 
+pub async fn check_price(item_url: Url) -> Result<Money, Box<dyn std::error::Error>> {
+    let body = reqwest::get(item_url.clone()).await?.text().await?;
+    let doc = Html::parse_document(&body);
+    let selector = Selector::parse("#priceblock_ourprice").unwrap(); 
+    for element in doc.select(&selector){
+        let mut price = element.text().collect::<Vec<_>>()[0].to_string();
+        price.remove(0);
+        println!("price: {}", price);
+        return Ok(Money::from_str(&price, "USD").unwrap());
+    }
+    return Ok(Money::from_str("0.00", "USD").unwrap());
+}
+
 impl WantedThing {
-    pub fn new(name: String, url: String, duration: Duration) -> Self {
+    pub fn new(name: String, item_url: String, duration: Duration) -> Self {
+        let parsed_url = Url::parse(&item_url).unwrap();
         return WantedThing {
             name: name,
-            url: Url::parse(&url).unwrap(),
+            item_url: parsed_url.clone(),
+            price: block_on(check_price(parsed_url.clone())).unwrap(),
             time_to_check: Utc::now() + duration,
         }
     }
     pub fn details(&self) {
         println!("Name: {}", self.name);
-        println!("URL: {}", self.url);
+        println!("URL: {}", self.item_url);
+        println!("Price: {}", self.price);
         println!("Time to check: {}", self.time_to_check);
     }
 
     pub fn has_check_passed(&self) -> bool {
         return Utc::now() > self.time_to_check;
-    }
-
-    pub async fn check_url(&self) -> Result<(), Box<dyn std::error::Error>> {
-        let body = reqwest::get(self.url.clone()).await?.text().await?;
-        let doc = Html::parse_document(&body);
-        let selector = Selector::parse("#priceblock_ourprice").unwrap(); 
-        for element in doc.select(&selector){
-            let price = element.text().collect::<Vec<_>>();
-            println!("{:?}", price);
-        }
-        Ok(())
     }
 }
