03f049a466c4678293e1d74d954934385c2cc89e
Jaret Screws <jaretscrews@gmail.com>
2020-12-18 01:16:06 -0500

    check to see if price has changed

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use chrono::Duration;
 
 pub mod wanted_thing;
@@ -8,12 +6,13 @@ use wanted_thing::WantedThing;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let test_item: WantedThing = WantedThing::new(
+    let mut test_item: WantedThing = WantedThing::new(
         "Watch Dogs Legion".to_string(), 
         "https://www.amazon.com/Watch-Dogs-Legion-PlayStation-5-Standard/dp/B08FS6BB9N/ref=sr_1_2?dchild=1&keywords=ps5+game&qid=1608266021&sr=8-2".to_string(), 
         Duration::seconds(2)
     );
 
     test_item.details();
+    test_item.check_for_price_drop().await?;
     Ok(())
 }
