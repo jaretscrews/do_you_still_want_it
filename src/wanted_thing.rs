0cdd3d633416015a39f2915fcd4f76ff665d5e39
Jaret Screws <jaretscrews@gmail.com>
2020-12-20 22:38:13 -0500

    now you only need to give wanted thing the url and it does the rest

--- src/wanted_thing.rs
+++ src/wanted_thing.rs
@@ -3,7 +3,7 @@ use chrono::{DateTime, Duration};
 use chrono::offset::Utc;
 use reqwest;
 use scraper::{Html, Selector};
-use rusty_money::Money;
+use rusty_money::{Money, Currency};
 use futures::executor::block_on;
 
 pub struct WantedThing {
@@ -13,9 +13,14 @@ pub struct WantedThing {
     time_to_check: DateTime<Utc>,
 }
 
-pub async fn check_price(item_url: Url) -> Result<Money, Box<dyn std::error::Error>> {
+//Pulls down the html to check to see if the price has changed
+//TODO move this into the struct
+async fn check_price(item_url: Url) -> Result<Money, Box<dyn std::error::Error>> {
+    //Get the html
     let body = reqwest::get(item_url.clone()).await?.text().await?;
+    //parse html
     let doc = Html::parse_document(&body);
+    //selector for the price
     let selector = Selector::parse("#priceblock_ourprice").unwrap(); 
     for element in doc.select(&selector){
         let mut price = element.text().collect::<Vec<_>>()[0].to_string();
@@ -26,15 +31,43 @@ pub async fn check_price(item_url: Url) -> Result<Money, Box<dyn std::error::Err
     return Ok(Money::from_str("0.00", "USD").unwrap());
 }
 
+//TODO move this into the struct
+async fn get_thing_from_link(item_url: Url, duration: Duration) -> Result<WantedThing, Box<dyn std::error::Error>> {
+    let mut name: String = "".to_string();
+    let mut price: Money = Money::new(0, Currency::from_string("USD".to_string()).unwrap());
+
+
+    //Get the html
+    let body = reqwest::get(item_url.clone()).await?.text().await?;
+    //parse that shit
+    let doc = Html::parse_document(&body);
+    //look for the price then the name
+    let selector = Selector::parse("#priceblock_ourprice").unwrap(); 
+    for element in doc.select(&selector){
+        let mut price_txt = element.text().collect::<Vec<_>>()[0].to_string();
+        price_txt.remove(0);
+        price = Money::from_str(&price_txt, "USD").unwrap();
+    }
+
+    let selector = Selector::parse("#productTitle").unwrap(); 
+    for element in doc.select(&selector){
+        name = element.text().collect::<Vec<_>>()[0].to_string();
+    }
+
+    //return the struct formed from the information in the doc
+    return Ok(WantedThing{
+        //remove extra white space from the names
+        name: name.replace("\n",""),
+        item_url: item_url,
+        price: price,
+        time_to_check: Utc::now() + duration,
+    })
+}
+
 impl WantedThing {
-    pub fn new(name: String, item_url: String, duration: Duration) -> Self {
+    pub fn new(item_url: String, duration: Duration) -> Self {
         let parsed_url = Url::parse(&item_url).unwrap();
-        return WantedThing {
-            name: name,
-            item_url: parsed_url.clone(),
-            price: block_on(check_price(parsed_url.clone())).unwrap(),
-            time_to_check: Utc::now() + duration,
-        }
+        return block_on(get_thing_from_link(parsed_url, duration)).unwrap();
     }
     pub fn details(&self) {
         println!("Name: {}", self.name);
